0d056d5aff80879e85455ab30f753d22d64d4061
Jerry Li <18708317004@163.com>
2023-06-13 18:39:48 +0800

    update sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[0].resolve().as_posix())
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LumacheTest'
+copyright = '2023, Jerry'
+author = 'Jerry'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+        'sphinx.ext.duration',
+        'sphinx.ext.doctest',
+        'sphinx.ext.autodoc',
+        ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/usage.rst
@@ -0,0 +1,26 @@
+Usage
+=====
+
+.. _installation:
+
+Installation
+------------
+
+To use Lumache, first install it using pip:
+
+.. code-block:: console
+
+   (.venv) $ pip install lumache
+
+Creating recipes
+----------------
+
+you can use the ``lumache.get_random_ingredients()`` function:
+
+.. autofunction:: lumache.get_random_ingredients
+
+.. autoexception:: lumache.InvalidKindError
+
+>>> import lumache
+>>> lumache.get_random_ingredients()
+['shells', 'gorgonzola', 'parsley']
